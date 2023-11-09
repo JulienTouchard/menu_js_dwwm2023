a921d1f2b9f2ec064f31f03eecbf5d513328fa24
JulienTouchard <julien.touchard@free.fr>
2023-11-09 10:22:01 +0100

    fin

--- assets/js/modules/menu.js
+++ assets/js/modules/menu.js
@@ -1,17 +1,29 @@
+/* creation d'une fonction pour initialiser mon menu */
 const initMenu = () => {
+
     const ulPrincipal = document.createElement("ul");
     document.querySelector("#navMenu").append(ulPrincipal);
     menuEntries.forEach((element, index) => {
-        console.log(element.name);
-        console.log(index);
+
         const liPrincipal = document.createElement('li');
         liPrincipal.id = "liPrincipal" + index;
+        // ajout de css avec liPrincipal.classList.add("maClass")
         liPrincipal.textContent = element.name;
         ulPrincipal.append(liPrincipal);
         // au click creation d'un sous menu
         liPrincipal.addEventListener("click", () => {
             //verification d'un sous menu existant avec son id
-            if (!document.body.contains(document.querySelector("#subUl" + index))) {
+            if (document.body.contains(document.querySelector("#subUl" + index))) {
+
+                document.querySelector("#subUl" + index).remove()
+            } else {
+                // detruire tous les elements submenu qui peuvent exister
+                for (let i = 0; i < menuEntries.length; i++) {
+                    if (document.body.contains(document.querySelector("#subUl" + i))) {
+                        document.querySelector("#subUl" + i).remove();
+                    }
+                }
+                // derniere etape : je peux maintenant créer l'element lier à l'event
                 const subUl = document.createElement("ul");
                 subUl.id = "subUl" + index;
                 liPrincipal.append(subUl);
@@ -20,10 +32,7 @@ const initMenu = () => {
                     subLi.innerText = subelem.subName;
                     subUl.append(subLi);
                 })
-            } else {
-                
             }
-
             //verification d'un sous menu existant avec son id
         })
 
